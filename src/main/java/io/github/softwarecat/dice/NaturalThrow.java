ec48d7e6a30fb9c0c4d9096b4e54f9353adbf8a7
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2021-04-03 22:39:03 +1100

    Implement NaturalThrow class

--- src/main/java/io/github/softwarecat/dice/NaturalThrow.java
+++ src/main/java/io/github/softwarecat/dice/NaturalThrow.java
@@ -29,89 +29,74 @@ import io.github.softwarecat.Outcome;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
- * {@link Throw} is the superclass for the various throws of the dice. Each subclass is a different grouping of the numbers, based
- * on the rules for Craps.
+ * {@link NaturalThrow} is a subclass of {@link Throw} for the natural number, 7.
  */
-public abstract class Throw {
+public class NaturalThrow extends Throw {
 
     /**
-     * A {@link Set} of one-roll Outcomes that win with this throw.
-     */
-    public final Set<Outcome> outcomes;
-
-    /**
-     * One of the two die values, from 1 to 6.
-     */
-    public int diceOne;
-
-    /**
-     * The other of the two die values, from 1 to 6.
-     */
-    public int diceTwo;
-
-    /**
-     * Creates this {@link Throw}, {@link Outcome}s can be added later.
+     * Creates this {@link Throw}. The constraint is that diceOne + diceTwo = 7. If the constraint is not satisfied, simply
+     * raise an {@link Exception}.
+     * <p>
+     * This uses the superclass constructor to add appropriate {@link Outcome} for a {@link Throw} of 7.
      *
      * @param diceOne the value of one die
      * @param diceTwo the value of the other die
      */
-    public Throw(int diceOne, int diceTwo) {
+    public NaturalThrow(int diceOne, int diceTwo) {
         this(diceOne, diceTwo, new Outcome[]{});
     }
 
     /**
-     * Creates this {@link Throw}, and associates the given Stream of {@link Outcome}s that are winning propositions.
+     * Creates this {@link Throw}. The constraint is that diceOne + diceTwo = 7. If the constraint is not satisfied, simply
+     * raise an {@link Exception}.
+     * <p>
+     * This uses the superclass constructor to add appropriate {@link Outcome} for a {@link Throw} of 7.
      *
      * @param diceOne  the value of one die
      * @param diceTwo  the value of the other die
      * @param outcomes the various {@link Outcome}s for this {@link Throw}
      */
-    public Throw(int diceOne, int diceTwo, Outcome... outcomes) {
+    public NaturalThrow(int diceOne, int diceTwo, Outcome... outcomes) {
         this(diceOne, diceTwo, Arrays.asList(outcomes.clone()));
     }
 
     /**
-     * Creates this {@link Throw}, and associates the given {@link Collection} of {@link Outcome}s that are winning propositions.
+     * Creates this {@link Throw}. The constraint is that diceOne + diceTwo = 7. If the constraint is not satisfied, simply
+     * raise an {@link Exception}.
+     * <p>
+     * This uses the superclass constructor to add appropriate {@link Outcome} for a {@link Throw} of 7.
      *
      * @param diceOne  the value of one die
      * @param diceTwo  the value of the other die
      * @param outcomes the various {@link Outcome}s for this {@link Throw}
      */
-    public Throw(int diceOne, int diceTwo, Collection<Outcome> outcomes) {
-        this.diceOne = diceOne;
-        this.diceTwo = diceTwo;
-        this.outcomes = new HashSet<>(outcomes);
+    public NaturalThrow(int diceOne, int diceTwo, Collection<Outcome> outcomes) {
+        super(diceOne, diceTwo, outcomes);
+
+        if (diceOne + diceTwo != 7) {
+            throw new IllegalArgumentException("Dice one and dice two must sum to 7");
+        }
     }
 
     /**
-     * Returns true if diceOne is equal to diceTwo. This helps determine if hardways bets have been won or lost.
+     * A natural 7 is odd, and can never be made “the hard way”. This method always returns false.
      *
-     * @return true if diceOne is equal to diceTwo
+     * @return {@link Boolean#FALSE}
      */
+    @Override
     public boolean isHard() {
-        return diceOne == diceTwo;
+        return false;
     }
 
     /**
-     * Calls one of the {@link Game} state change methods: {@link Game#craps()}, {@link Game#natural()}, {@link Game#eleven()}, {@link Game#point()}. This may
-     * change the game state and resolve bets.
+     * Calls the {@link Game#natural()} method of a {@link Game}. This may change the game state and resolve bets.
      *
      * @param game the {@link Game} to be updated based on this throw
      */
-    public abstract void updateGame(Game game);
-
-    /**
-     * An easy-to-read {@link String} output method is also very handy. This should return a {@link String} representation of the
-     * dice. A form that looks like "1,2" works nicely.
-     *
-     * @return a {@link String} representation of the dice
-     */
     @Override
-    public String toString() {
-        return String.format("%d, %d", diceOne, diceTwo);
+    public void updateGame(Game game) {
+        game.natural();
     }
 }
